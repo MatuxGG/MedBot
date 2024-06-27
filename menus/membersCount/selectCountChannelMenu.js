88d9518972fc9e2b0c9914d94bd4262b38832e1b
martaud <matthieu.artaud.ext@corys.fr>
2024-06-27 14:25:44 +0200

    Members count now updated after command

--- menus/membersCount/selectCountChannelMenu.js
+++ menus/membersCount/selectCountChannelMenu.js
@@ -8,6 +8,14 @@ module.exports = {
         const selectChannelId = interaction.values[0];
         const message = await trans(guildId, 'count_channel_saved');
 
+        let membersText = await trans(guildId, 'members');
+        let membersCount = interaction.guild.members.cache.size;
+        let channel = interaction.guild.channels.cache.get(selectChannelId);
+        if (channel) {
+            channel.setName(`${membersCount} ${membersText}`);
+            console.log(`Updating members count in "${channel.name}" to ${membersCount}`);
+        }
+
         Guild.findOneAndUpdate( { id: interaction.guild.id }, { membersCountChannel: selectChannelId }, function (err, weekCalendar) {
             interaction.reply({content: message, ephemereal: true});
         });
