f3ea9b11b95c1196a5b17c352d81371e401cf4f7
martaud <matthieu.artaud.ext@corys.fr>
2024-09-17 15:19:51 +0200

    Version 1.1.4 - CI pipeline

--- restartBot.sh
+++ restartBot.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ps -ef | grep "node medbot.js" | grep -v grep | awk '{print $2}' | xargs kill
+# Récupérer le PID du processus
+PID=$(ps -ef | grep "node medbot.js" | grep -v grep | awk '{print $2}')
+
+# Vérifier si PID est vide
+if [ -z "$PID" ]; then
+    echo "Aucun processus 'node medbot.js' trouvé."
+else
+    # Tuer le processus
+    echo "Processus trouvé : $PID. Terminaison..."
+    echo $PID | xargs kill
+fi
